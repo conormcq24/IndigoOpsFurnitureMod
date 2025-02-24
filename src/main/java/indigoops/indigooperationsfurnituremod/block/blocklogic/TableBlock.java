50f967cbf9f201266dd9dfabc90ba4f1c52f64ae
Conor <conormcq24@gmail.com>
2025-02-23 21:29:51 -0600

    added the proper logic to handle rotated table placements so that the block placed is always the bottom left quadrant of the table regardless of direction faced

--- src/main/java/indigoops/indigooperationsfurnituremod/block/blocklogic/TableBlock.java
+++ src/main/java/indigoops/indigooperationsfurnituremod/block/blocklogic/TableBlock.java
@@ -1,14 +1,12 @@
 package indigoops.indigooperationsfurnituremod.block.blocklogic;
 
-import net.minecraft.block.Block;
-import net.minecraft.block.BlockState;
-import net.minecraft.block.Blocks;
-import net.minecraft.block.ShapeContext;
+import net.minecraft.block.*;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemPlacementContext;
 import net.minecraft.item.ItemStack;
 import net.minecraft.state.StateManager;
+import net.minecraft.state.property.DirectionProperty;
 import net.minecraft.state.property.EnumProperty;
 import net.minecraft.util.StringIdentifiable;
 import net.minecraft.util.math.BlockPos;
@@ -19,6 +17,55 @@ import net.minecraft.world.WorldView;
 import org.jetbrains.annotations.Nullable;
 
 public class TableBlock extends Block {
+    // Enum to track what type of wood the table is composed of for texturing purposes
+    public enum TableWood implements StringIdentifiable {
+        ACACIA("acacia"),
+        BIRCH("birch"),
+        CHERRY("cherry"),
+        CRIMSON("crimson"),
+        DARKOAK("dark_oak"),
+        JUNGLE("jungle"),
+        MANGROVE("mangrove"),
+        OAK("oak"),
+        SPRUCE("spruce"),
+        WARPED("warped");
+
+        private final String woodType;
+
+        TableWood(String woodType) { this.woodType = woodType; }
+
+        @Override
+        public String asString() { return this.woodType; }
+
+    }
+    // Enum to track if the table has a cloth and what color it is if it does, for texturing purposes
+    public enum TableCloth implements StringIdentifiable {
+        NONE("none"),
+        WHITE("white"),
+        LIGHTGRAY("light_gray"),
+        GRAY("gray"),
+        BLACK("black"),
+        BROWN("brown"),
+        RED("red"),
+        ORANGE("orange"),
+        YELLOW("yellow"),
+        LIME("lime"),
+        GREEN("green"),
+        CYAN("cyan"),
+        LIGHTBLUE("light_blue"),
+        BLUE("blue"),
+        PURPLE("purple"),
+        MAGENTA("magenta"),
+        PINK("pink");
+
+        private final String color;
+
+        TableCloth(String color) { this.color = color; }
+
+        @Override
+        public String asString() { return this.color; }
+
+    }
 
     // Enum to track which quarter of the table this block represents
     public enum TablePart implements StringIdentifiable {
@@ -41,15 +88,16 @@ public class TableBlock extends Block {
 
 
     public static final EnumProperty<TablePart> PART = EnumProperty.of("part", TablePart.class);
+    public static final DirectionProperty FACING = HorizontalFacingBlock.FACING;
 
     public TableBlock(Settings settings) {
         super(settings.nonOpaque());
-        setDefaultState(getStateManager().getDefaultState().with(PART, TablePart.TOP_LEFT));
+        setDefaultState(getStateManager().getDefaultState().with(PART, TablePart.TOP_LEFT).with(FACING, Direction.NORTH));
     }
 
     @Override
     protected void appendProperties(StateManager.Builder<Block, BlockState> builder) {
-        builder.add(PART);
+        builder.add(PART, FACING);
     }
 
     @Nullable
@@ -57,30 +105,81 @@ public class TableBlock extends Block {
     public BlockState getPlacementState(ItemPlacementContext ctx) {
         BlockPos pos = ctx.getBlockPos();
         World world = ctx.getWorld();
+        Direction facing = ctx.getHorizontalPlayerFacing();
 
-        // Check if we have space for all four blocks
-        if (canPlaceTable(world, pos)) {
-            return getDefaultState().with(PART, TablePart.BOTTOM_LEFT);
+        // Check if we have space for all four blocks using the facing direction
+        if (canPlaceTable(world, pos, facing)) {
+            return getDefaultState().with(PART, TablePart.BOTTOM_LEFT).with(FACING, facing);
         }
 
         return null;
     }
 
-    private boolean canPlaceTable(World world, BlockPos pos) {
-        return world.getBlockState(pos).isAir()
-                && world.getBlockState(pos.east()).isAir()
-                && world.getBlockState(pos.north()).isAir()
-                && world.getBlockState(pos.north().east()).isAir();
+    private boolean canPlaceTable(World world, BlockPos pos, Direction facing) {
+        // Check if the four parts of the table can be placed based on the facing direction
+        switch (facing) {
+            case NORTH:
+                return world.getBlockState(pos).isAir()
+                        && world.getBlockState(pos.east()).isAir()
+                        && world.getBlockState(pos.north()).isAir()
+                        && world.getBlockState(pos.north().east()).isAir();
+            case EAST:
+                return world.getBlockState(pos).isAir()
+                        && world.getBlockState(pos.south()).isAir()
+                        && world.getBlockState(pos.east()).isAir()
+                        && world.getBlockState(pos.south().east()).isAir();
+            case SOUTH:
+                return world.getBlockState(pos).isAir()
+                        && world.getBlockState(pos.west()).isAir()
+                        && world.getBlockState(pos.south()).isAir()
+                        && world.getBlockState(pos.south().west()).isAir();
+            case WEST:
+                return world.getBlockState(pos).isAir()
+                        && world.getBlockState(pos.north()).isAir()
+                        && world.getBlockState(pos.west()).isAir()
+                        && world.getBlockState(pos.north().west()).isAir();
+            default:
+                return false;
+        }
     }
+
 
     @Override
     public void onPlaced(World world, BlockPos pos, BlockState state, @Nullable LivingEntity placer, ItemStack itemStack) {
         if (!world.isClient) {
-            // Place all four parts of the table
-            world.setBlockState(pos, state.with(PART, TablePart.BOTTOM_LEFT), Block.NOTIFY_ALL);
-            world.setBlockState(pos.east(), state.with(PART, TablePart.BOTTOM_RIGHT), Block.NOTIFY_ALL);
-            world.setBlockState(pos.north(), state.with(PART, TablePart.TOP_LEFT), Block.NOTIFY_ALL);
-            world.setBlockState(pos.north().east(), state.with(PART, TablePart.TOP_RIGHT), Block.NOTIFY_ALL);
+            Direction facing = state.get(FACING); // Get the player's facing direction
+
+            // Rotate the table parts based on the facing direction
+            switch (facing) {
+                case NORTH:
+                    // Facing north: Place the table as normal
+                    world.setBlockState(pos, state.with(PART, TablePart.BOTTOM_LEFT), Block.NOTIFY_ALL);
+                    world.setBlockState(pos.east(), state.with(PART, TablePart.BOTTOM_RIGHT), Block.NOTIFY_ALL);
+                    world.setBlockState(pos.north(), state.with(PART, TablePart.TOP_LEFT), Block.NOTIFY_ALL);
+                    world.setBlockState(pos.north().east(), state.with(PART, TablePart.TOP_RIGHT), Block.NOTIFY_ALL);
+                    break;
+                case EAST:
+                    // Facing east: Rotate the table 90 degrees
+                    world.setBlockState(pos, state.with(PART, TablePart.BOTTOM_LEFT), Block.NOTIFY_ALL);
+                    world.setBlockState(pos.south(), state.with(PART, TablePart.BOTTOM_RIGHT), Block.NOTIFY_ALL);
+                    world.setBlockState(pos.east(), state.with(PART, TablePart.TOP_LEFT), Block.NOTIFY_ALL);
+                    world.setBlockState(pos.south().east(), state.with(PART, TablePart.TOP_RIGHT), Block.NOTIFY_ALL);
+                    break;
+                case SOUTH:
+                    // Facing south: Rotate the table 180 degrees
+                    world.setBlockState(pos, state.with(PART, TablePart.BOTTOM_LEFT), Block.NOTIFY_ALL);
+                    world.setBlockState(pos.west(), state.with(PART, TablePart.BOTTOM_RIGHT), Block.NOTIFY_ALL);
+                    world.setBlockState(pos.south(), state.with(PART, TablePart.TOP_LEFT), Block.NOTIFY_ALL);
+                    world.setBlockState(pos.south().west(), state.with(PART, TablePart.TOP_RIGHT), Block.NOTIFY_ALL);
+                    break;
+                case WEST:
+                    // Facing west: Rotate the table 270 degrees
+                    world.setBlockState(pos, state.with(PART, TablePart.BOTTOM_LEFT), Block.NOTIFY_ALL);
+                    world.setBlockState(pos.north(), state.with(PART, TablePart.BOTTOM_RIGHT), Block.NOTIFY_ALL);
+                    world.setBlockState(pos.west(), state.with(PART, TablePart.TOP_LEFT), Block.NOTIFY_ALL);
+                    world.setBlockState(pos.north().west(), state.with(PART, TablePart.TOP_RIGHT), Block.NOTIFY_ALL);
+                    break;
+            }
         }
     }
 
@@ -97,32 +196,97 @@ public class TableBlock extends Block {
         return super.getStateForNeighborUpdate(state, direction, neighborState, world, pos, neighborPos);
     }
 
-    private boolean isTablePart(WorldAccess world, BlockPos pos, TablePart part) {
-        BlockState state = world.getBlockState(pos);
-        return state.isOf(this) && state.get(PART) == part;
-    }
-
     @Override
     public BlockState onBreak(World world, BlockPos pos, BlockState state, PlayerEntity player) {
         if (!world.isClient && player.isCreative()) {
             TablePart part = state.get(PART);
+            Direction facing = state.get(FACING);
 
-            // Get the northwest corner position
-            BlockPos bottomLeftPos = switch (part) {
-                case BOTTOM_LEFT -> pos;
-                case BOTTOM_RIGHT -> pos.west();
-                case TOP_LEFT -> pos.south();
-                case TOP_RIGHT -> pos.south().west();
-            };
+            // Define the array of BlockPos for all four parts of the table, based on the facing direction and part
+            BlockPos[] positionsToBreak = getPositionsForFacing(part, pos, facing);
 
-            // Break all parts
-            breakPart(world, bottomLeftPos);
-            breakPart(world, bottomLeftPos.east());
-            breakPart(world, bottomLeftPos.north());
-            breakPart(world, bottomLeftPos.north().east());
+            // Loop through all the positions to break the corresponding table parts
+            for (BlockPos blockPos : positionsToBreak) {
+                breakPart(world, blockPos);
+            }
         }
 
         return super.onBreak(world, pos, state, player);
+    }
+
+    private BlockPos[] getPositionsForFacing(TablePart part, BlockPos pos, Direction facing) {
+        switch (facing) {
+            case NORTH:
+                return getPositionsForFacingNorth(part, pos);
+            case EAST:
+                return getPositionsForFacingEast(part, pos);
+            case SOUTH:
+                return getPositionsForFacingSouth(part, pos);
+            case WEST:
+                return getPositionsForFacingWest(part, pos);
+            default:
+                return new BlockPos[] { pos }; // default case for any unknown directions
+        }
+    }
+
+    private BlockPos[] getPositionsForFacingNorth(TablePart part, BlockPos pos) {
+        switch (part) {
+            case BOTTOM_LEFT:
+                return new BlockPos[] { pos, pos.east(), pos.north(), pos.north().east() };
+            case BOTTOM_RIGHT:
+                return new BlockPos[] { pos, pos.west(), pos.north(), pos.north().west() };
+            case TOP_LEFT:
+                return new BlockPos[] { pos, pos.east(), pos.south(), pos.south().east() };
+            case TOP_RIGHT:
+                return new BlockPos[] { pos, pos.west(), pos.south(), pos.south().west() };
+            default:
+                return new BlockPos[] { pos };
+        }
+    }
+
+    private BlockPos[] getPositionsForFacingEast(TablePart part, BlockPos pos) {
+        switch (part) {
+            case BOTTOM_LEFT:
+                return new BlockPos[] { pos, pos.south(), pos.east(), pos.south().east() };
+            case BOTTOM_RIGHT:
+                return new BlockPos[] { pos, pos.north(), pos.east(), pos.north().east() };
+            case TOP_LEFT:
+                return new BlockPos[] { pos, pos.south(), pos.west(), pos.south().west() };
+            case TOP_RIGHT:
+                return new BlockPos[] { pos, pos.north(), pos.west(), pos.north().west() };
+            default:
+                return new BlockPos[] { pos };
+        }
+    }
+
+    private BlockPos[] getPositionsForFacingSouth(TablePart part, BlockPos pos) {
+        switch (part) {
+            case BOTTOM_LEFT:
+                return new BlockPos[] { pos, pos.west(), pos.south(), pos.south().west() };
+            case BOTTOM_RIGHT:
+                return new BlockPos[] { pos, pos.east(), pos.south(), pos.south().east() };
+            case TOP_LEFT:
+                return new BlockPos[] { pos, pos.west(), pos.north(), pos.north().west() };
+            case TOP_RIGHT:
+                return new BlockPos[] { pos, pos.east(), pos.north(), pos.north().east() };
+            default:
+                return new BlockPos[] { pos };
+        }
+    }
+
+    private BlockPos[] getPositionsForFacingWest(TablePart part, BlockPos pos) {
+        switch (part) {
+            case BOTTOM_LEFT:
+                return new BlockPos[] { pos, pos.north(), pos.west(), pos.north().west() };
+            case BOTTOM_RIGHT:
+                return new BlockPos[] { pos, pos.south(), pos.west(), pos.south().west() };
+            case TOP_LEFT:
+                return new BlockPos[] { pos, pos.north(), pos.east(), pos.north().east() };
+            case TOP_RIGHT:
+                return new BlockPos[] { pos, pos.south(), pos.east(), pos.south().east() };
+            default:
+                return new BlockPos[] { pos };
+        }
     }
 
     private void breakPart(World world, BlockPos pos) {
@@ -131,4 +295,6 @@ public class TableBlock extends Block {
             world.setBlockState(pos, Blocks.AIR.getDefaultState(), Block.NOTIFY_ALL | Block.SKIP_DROPS);
         }
     }
+
+
 }
